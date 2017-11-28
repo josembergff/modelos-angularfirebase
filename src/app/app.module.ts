56623e1398152e70e890054b5613bd2870e788a8
josembergff <josembergff@gmail.com>
2017-11-28 20:42:22 -0200

    configuração de fireabase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyDi2YOCedTijFZ-UqVHfKI-kN2lLcnJjIc',
+  authDomain: 'vife-votos.firebaseapp.com',
+  databaseURL: 'https://vife-votos.firebaseio.com',
+  projectId: 'vife-votos',
+  storageBucket: '',
+  messagingSenderId: '872271470'
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AngularFireModule.initializeApp(firebaseConfig)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
